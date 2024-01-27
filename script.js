22f6afeb2e808fa3627afd5b00dbc181c8bc3051
mubashirnazir42 <153961579+mubashirnazir42@users.noreply.github.com>
2024-01-27 23:06:04 +0500

    firstcommit

---
+++ script.js
@@ -0,0 +1,51 @@
+// "use strict";
+
+// // let lastName = "nazir";
+// // let oldLastName = lastName;
+// // lastName = "hussain";
+
+// // let devil = 5;
+// // let ios = "hi";
+
+// // let jonas = {
+// //   name: "mubashir",
+// //   calcAge: function () {
+// //     const printName = () => {
+// //       console.log(this.name);
+// //     };
+// //     printName();
+// //   },
+// // };
+// // const mubashir = jonas;
+
+// // mubashir.name = "ali";
+// // jonas.name = "waseem";
+
+// // mubashir.calcAge();
+
+// // jonas.calcAge();
+// // console.log(oldLastName);
+// // console.log(lastName);
+
+// const me = {
+//   name: "mubashir",
+//   class: "23",
+//   age: 24,
+//   calcAge: function () {
+//     console.log(this.name);
+//   },
+// };
+
+// me.calcAge();
+
+// const jonas = Object.assign(
+//   {
+//     add: 23,
+//   },
+//   me
+// );
+// me.name = "ali";
+// me.class = "namrood";
+
+// console.log(me);
+// console.log(jonas);
